65273b6ba711e224eccc026b230b160928c4de6e
Chris Brown <cbrown1234@hotmail.co.uk>
2023-12-02 14:16:43 +0000

    Add Day 1 Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
 fn main() -> Result<()> {
-    let file = File::open("input_data.txt")?;
+    let file = File::open("inputs/day1.txt")?;
     let buf_reader = BufReader::new(file);
     let lines = buf_reader.lines();
 
@@ -26,5 +27,60 @@ fn main() -> Result<()> {
         })
         .sum();
     println!("Calibration = {calibration}");
+
+    let numbers = HashMap::from([
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+
+    let file = File::open("inputs/day1.txt")?;
+    let buf_reader = BufReader::new(file);
+    let lines = buf_reader.lines();
+
+    let calibration: u32 = lines
+        .map(|line| line.expect("able to read file"))
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            eprintln!("{line}");
+            let positions: Vec<_> = numbers
+                .iter()
+                .flat_map(|(k, number)| line.match_indices(k).map(|(pos, _)| (number.to_owned(), pos)))
+                .collect();
+
+            let first = positions
+                .iter()
+                .min_by_key(|(_number, pos)| pos)
+                .expect("first number to exist");
+            let last = positions
+                .iter()
+                .max_by_key(|(_number, pos)| pos)
+                .unwrap_or(first);
+            (first.0, last.0)
+        })
+        .map(|(first, last)| {
+            let number = (10 * first) + last;
+            eprintln!("{number}");
+            number
+        })
+        .sum();
+    println!("Calibration part 2 = {calibration}");
     Ok(())
 }
